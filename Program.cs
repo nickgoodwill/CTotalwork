c8432ebdedce51acfed4e0f18d2483e00d23bd48
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-28 10:57:57 +0300

    C# task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+//  длина которых меньше, либо равна 3 символам. 
+//  Первоначальный массив можно ввести с клавиатуры, 
+//  либо задать на старте выполнения алгоритма. и
+//  При решении не рекомендуется пользоваться коллекциям, 
+//  лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string [] stringArray = new string [m];
+
+void array(string [] stringArray)
+{
+  for (int i = 0; i<stringArray.Length; i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     stringArray[i] = Console.ReadLine();
+  }
+}
+
+string [] symbol(string [] stringArray)
+{
+  int n = 0;
+  for (int i = 0; i<stringArray.Length; i++)
+  {
+    if(stringArray[i].Length <=3)
+    n++;
+  }
+  string [] rez = new string [n];
+  int j = 0;
+  for (int i = 0; i<stringArray.Length; i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        rez[j] = stringArray[i];
+        j++;
+    }
+  }
+  return rez;
+}
+void printA(string [] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0; i<stringArray.Length - 1; i++)
+    {
+      Console.Write(stringArray[i] + ", ");
+    }
+    Console.WriteLine(stringArray[stringArray.Length -1] + "]");
+}
+array(stringArray);
+printA(symbol(stringArray));
+
